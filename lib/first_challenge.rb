e04185d25070b15c0fe775d475722b3424f4ce2e
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 02:16:06 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,7 @@ def first_challenge
     }
   }
 
-  contacts["Freddy Mercury"][:favorite_icecream_flavors].keep_if { |flavour| flavour !=   }
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].keep_if { |flavour| flavour != "strawberry"  }
 
 
   #remember to return your newly altered contacts hash!
